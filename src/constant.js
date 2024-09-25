b56636b5533adf3d187ed3b2d6e05d5ccf71555a
jawad-ilyas <mughaljawad12@gmail.com>
2024-09-25 23:39:14 +0500

    changing intourl

--- src/constant.js
+++ src/constant.js
@@ -1,3 +1,3 @@
 
-// export const ApiUrl = "https://build-and-deploy-a-fullsta-git-bf538e-jawadmughal0079s-projects.vercel.app"
-export const ApiUrl = "http://localhost:8080/api/v1"
+export const ApiUrl = "https://build-and-deploy-a-fullsta-git-bf538e-jawadmughal0079s-projects.vercel.app/api/v1"
+// export const ApiUrl = "http://localhost:8080/api/v1"
