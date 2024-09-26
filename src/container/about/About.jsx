bdb8c2b0025b092d8f17f221e7d36cf90e42ceda
jawad-ilyas <mughaljawad12@gmail.com>
2024-09-27 01:29:36 +0500

    improve the about

--- src/container/about/About.jsx
+++ src/container/about/About.jsx
@@ -3,8 +3,28 @@ import AppWrapper from "../../wrapper/AppWrap"
 import { images } from "../../constants"
 import Container from "../Container"
 import MotionWrap from "../../wrapper/MotionWrap"
+import axios from "axios"
+import { ApiUrl } from "../../constant"
+import { useEffect } from "react"
 const About = () => {
 
+  const [about, setAbout] = useEffect([])
+
+  const fetchWork = async () => {
+
+    await axios.get(`${ApiUrl}/about/fetchAbout`)
+      .then((response) => {
+        console.log("Work js :: fetchWork :: response", response)
+        setAbout(response?.data?.data)
+      
+      })
+  }
+
+  useEffect(() => {
+
+    fetchWork();
+
+  }, [])
   const abouts = [
     { title: "web development ", description: "I am a good web developer ", imgUrl: images.about01 },
     { title: "web Design ", description: "I am a good web developer ", imgUrl: images.about02 },
@@ -34,11 +54,11 @@ const About = () => {
               whileHover={{ scale: 1.1 }}
               transition={{ duration: 0.5, type: 'tween' }}
               className="app_profiles-item w-full  flex justify-start items-start flex-col  cursor-pointer"
-              key={about.title + index}
+              key={about.aboutName + index}
             >
-              <img className="w-full object-cover h-42 rounded-md" src={about.imgUrl} alt={about.title} />
-              <h2 className="bold-text capitalize font-semibold text-2xl" style={{ marginTop: 20 }}>{about.title}</h2>
-              <p className="p-text font-light text-base" style={{ marginTop: 4 }}>{about.description}</p>
+              <img className="w-full object-cover h-42 rounded-md" src={about.aboutImgUrl} alt={about.aboutName} />
+              <h2 className="bold-text capitalize font-semibold text-2xl" style={{ marginTop: 20 }}>{about.aboutName}</h2>
+              <p className="p-text font-light text-base" style={{ marginTop: 4 }}>{about.aboutDescription}</p>
             </motion.div>
           ))}
         </div>
@@ -48,4 +68,4 @@ const About = () => {
   )
 }
 
-export default AppWrapper(MotionWrap(About) , "about")
+export default AppWrapper(MotionWrap(About), "about")
